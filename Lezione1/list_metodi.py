2c7c771f5d2c38b8abc59e02ae2b5bc04e68d71a
Camilla Corno <203082996+camillac-bit@users.noreply.github.com>
2026-03-22 18:46:15 +0100

    Esercizio 3: list e metodi

---
+++ Lezione1/list_metodi.py
@@ -0,0 +1,16 @@
+# ESERCIZIO 3:
+# Data la lista names = ['Thor', 'Batman', 'Iron Man']
+# ordinare la lista in ordine lessicografico e stampare la lista.
+# In seguito ordinare la lista in ordine non crescente rispetto alla lunghezza
+# delle stringhe contenute in essa. Stampare nuovamente la lista.
+
+
+names = ['Thor', 'Batman', 'Iron Man']
+
+#names.sort() restituisce None
+
+names.sort()
+print(names)
+
+names.sort(key=lambda n : len(n), reverse=True)
+print(names)
